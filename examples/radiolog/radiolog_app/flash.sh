f1f2f0421d020a75b8b383ace13d9067ba704c7e
Daniele Basile <asterix24@gmail.com>
2021-08-03 22:01:09 +0200

    main: go more fast, and exit if fail

--- examples/radiolog/radiolog_app/flash.sh
+++ examples/radiolog/radiolog_app/flash.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+set -uo pipefail
 
-make
+make -j8 || exit 1
 
 cp build/radiolog_app.bin .
 openssl s_server -WWW -key server_certs/ca_key.pem -cert server_certs/ca_cert.pem -port 8070
