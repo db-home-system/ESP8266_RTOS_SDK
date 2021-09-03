5e21adaa6197d6c26b6cbf80be351167f11865b7
Daniele Basile <asterix24@gmail.com>
2021-09-03 18:33:45 +0200

    utils: add to send configuration file

--- examples/radiolog/node.py
+++ examples/radiolog/node.py
@@ -11,6 +11,7 @@ def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
     client.subscribe("radiolog/#")
 
+
 client = mqtt.Client()
 client.username_pw_set("radiolog", "home24radiolog98")
 client.on_connect = on_connect
@@ -20,37 +21,40 @@ client.connect("fagotto.asterix.cloud", 1317, 60)
 
 cmds = [
     "reset",
-    "open" ,
+    "open",
     "close",
-    "go"   ,
-    "r"    ,
-    "w"    ,
+    "stop",
+    "go",
+    "r",
+    "w",
     "dump",
     "sw",
+    "log",
+    "file",
 ]
 
 
 NODEIDs = [
-        "Node_85d904",
-        "Node_f4a98f",
-        "Node_513b66"
-        ]
-TOPIC=0
-PAYLOAD=1
+    "Node_85d904",
+    "Node_f4a98f",
+    "Node_513b66"
+]
+TOPIC = 0
+PAYLOAD = 1
 
-node_list = ["%s: idx=%s" % (n,i) for i,n in enumerate(NODEIDs)]
+node_list = ["%s: idx=%s" % (n, i) for i, n in enumerate(NODEIDs)]
 
 if len(sys.argv) < 3:
-    print("usage %s nodeid <%s> cmd <%s>" % (sys.argv[0], list(node_list),
-        cmds))
+    print("usage %s <nodeid [%s]> <cmd [%s]>" % (sys.argv[0], list(node_list),
+                                                 cmds))
     sys.exit(0)
 
+nodeid = sys.argv[1].strip()
+key = sys.argv[2].strip()
 try:
-    nodeid = sys.argv[1].strip()
-    key = sys.argv[2].strip()
     cmd_args = sys.argv[3].strip()
 except IndexError:
-    pass
+    cmd_args = None
 
 try:
     NODEID = NODEIDs[int(nodeid)]
@@ -59,32 +63,18 @@ except IndexError:
     sys.exit(1)
 
 cmd_table = {
-   cmds[0] : ["radiolog/%s/reset" % NODEID, ""],
-   cmds[1] : ["radiolog/%s/cover/set" % NODEID, "open"],
-   cmds[2] : ["radiolog/%s/cover/set" % NODEID, "close"],
-   cmds[3] : ["radiolog/%s/cover/set/position" % NODEID, "ARGS"],
-   cmds[4] : ["radiolog/%s/cfg/read" % NODEID, "ARGS"],
-   cmds[5] : ["radiolog/%s/cfg/write" % NODEID, "ARGS"],
-   cmds[6] : ["radiolog/%s/cfg/dump" % NODEID, ""],
-   cmds[7] : ["radiolog/%s/switch/set" % NODEID, "ARGS"],
+    cmds[0]: ["radiolog/%s/reset" % NODEID, ""],
+    cmds[1]: ["radiolog/%s/cover/set" % NODEID, "open"],
+    cmds[2]: ["radiolog/%s/cover/set" % NODEID, "close"],
+    cmds[3]: ["radiolog/%s/cover/set" % NODEID, "stop"],
+    cmds[4]: ["radiolog/%s/cover/set/position" % NODEID, "ARGS"],
+    cmds[5]: ["radiolog/%s/cfg/read" % NODEID, "ARGS"],
+    cmds[6]: ["radiolog/%s/cfg/write" % NODEID, "ARGS"],
+    cmds[7]: ["radiolog/%s/cfg/dump" % NODEID, ""],
+    cmds[8]: ["radiolog/%s/switch/set" % NODEID, "ARGS"],
+    cmds[9]: [],
+    cmds[10]: [],
 }
-
-cmd_table_cfg = [
-    ["radiolog/%s/cfg/write" % NODEID , "cover_open:1"] ,
-    ["radiolog/%s/cfg/read" % NODEID  , "cover_open"] ,
-
-    ["radiolog/%s/cfg/write" % NODEID , "cover_close:0"] ,
-    ["radiolog/%s/cfg/read" % NODEID  , "cover_close"] ,
-
-    ["radiolog/%s/cfg/write" % NODEID , "cover_up_time:25"] ,
-    ["radiolog/%s/cfg/read" % NODEID  , "cover_up_time"] ,
-
-    ["radiolog/%s/cfg/write" % NODEID , "cover_down_time:25"] ,
-    ["radiolog/%s/cfg/read" % NODEID  , "cover_down_time"] ,
-
-    ["radiolog/%s/cfg/write" % NODEID , "cover_polling_time:250"] ,
-    ["radiolog/%s/cfg/read" % NODEID  , "cover_polling_time"] ,
-]
 
 
 def on_message(client, userdata, msg):
@@ -97,12 +87,27 @@ if key == "log":
     client.loop_forever()
 client.loop_start()
 
-if key == "cfg":
-    for i in cmd_table_cfg:
-        client.publish(i[TOPIC], i[PAYLOAD])
-        time.sleep(1)
+if key == 'file':
+    client.on_message = on_message
+    if cmd_args is None:
+        print("You should specify a file to send")
+        sys.exit(1)
 
+    with open(cmd_args, 'r') as c:
+        topic = None
+        for line in c:
+            line = line.strip()
+            if line.startswith("#"):
+                continue
+            if "TOPIC" in line:
+                topic = line.split("=")[1].strip()
+                topic = "radiolog/%s/%s" % (NODEID, topic)
+                continue
+            if topic is not None:
+                client.publish(topic, line)
+            time.sleep(1)
     sys.exit(0)
+
 
 if key in cmd_table:
     print(key)
