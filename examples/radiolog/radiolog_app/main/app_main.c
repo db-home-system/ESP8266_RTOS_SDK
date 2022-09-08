8d6dc0adc1d2ae24ca6e01d4786e3cc842509967
Daniele Basile <asterix24@gmail.com>
2022-09-08 22:01:44 +0200

    Clean up

--- examples/radiolog/radiolog_app/main/app_main.c
+++ examples/radiolog/radiolog_app/main/app_main.c
@@ -40,7 +40,6 @@
 static const char *TAG = "Radiolog";
 
 static QueueHandle_t mqtt_msg_queue;
-static QueueHandle_t syslog_queue;
 
 void cmd_reset(const char *topic, size_t len_topic, const char *data, size_t len_data) {
     esp_restart();
@@ -72,73 +71,6 @@ static void publish_msg(void * pvParameter)
 
 #define UDP_PORT 3333
 
-static void syslog(void * pvParameter)
-{
-    while (1) {
-        /*
-        uint8_t buff[128];
-        int sock;
-        sock = create_multicast_ipv4_socket();
-        if (sock < 0) {
-            ESP_LOGE(TAG, "Failed to create IPv4 multicast socket");
-        }
-        static int send_count;
-        const char sendfmt[] = "Multicast #%d sent by ESP32\n";
-        char sendbuf[48];
-        char addrbuf[32] = { 0 };
-        int len = snprintf(sendbuf, sizeof(sendbuf), sendfmt, send_count++);
-        if (len > sizeof(sendbuf)) {
-            ESP_LOGE(TAG, "Overflowed multicast sendfmt buffer!!");
-            send_count = 0;
-            err = -1;
-        }
-
-        struct addrinfo hints = {
-            .ai_flags = AI_PASSIVE,
-            .ai_socktype = SOCK_DGRAM,
-        };
-        struct addrinfo *res;
-
-
-        hints.ai_family = AF_INET; // For an IPv4 socket
-        int err = getaddrinfo(CONFIG_EXAMPLE_MULTICAST_IPV4_ADDR,
-                              NULL,
-                              &hints,
-                              &res);
-        if (err < 0) {
-            ESP_LOGE(TAG, "getaddrinfo() failed for IPV4 destination address. error: %d", err);
-            break;
-        }
-        if (res == 0) {
-            ESP_LOGE(TAG, "getaddrinfo() did not return any addresses");
-            break;
-        }
-                ((struct sockaddr_in *)res->ai_addr)->sin_port = htons(UDP_PORT);
-        inet_ntoa_r(((struct sockaddr_in *)res->ai_addr)->sin_addr, addrbuf, sizeof(addrbuf)-1);
-        ESP_LOGI(TAG, "Sending to IPV4 multicast address %s:%d...",  addrbuf, UDP_PORT);
-        err = sendto(sock, sendbuf, len, 0, res->ai_addr, res->ai_addrlen);
-        freeaddrinfo(res);
-        if (err < 0) {
-            ESP_LOGE(TAG, "IPV4 sendto failed. errno: %d", errno);
-            break;
-        }
-                ESP_LOGE(TAG, "Shutting down socket and restarting...");
-        shutdown(sock, 0);
-        close(sock);
-
-        if(xQueueReceive(syslog_queue, buff, (TickType_t)0))
-        {
-            int err = sendto(sock, payload, strlen(payload), 0, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
-            if (err < 0) {
-                ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
-            }
-            ESP_LOGI(TAG, "Message sent");
-        }
-        */
-        DELAY_MS(100);
-    }
-}
-
 static void event_cover_stop(const cover_ctx_t *ctx) {
     cover_prepareStatusMsg(ctx);
 }
@@ -149,9 +81,6 @@ void app_main(void)
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     ESP_ERROR_CHECK(common_connect());
-
-    syslog_queue = xQueueCreate(5, 128);
-    xTaskCreate(&syslog, "syslog_task", 8192, NULL, 10, NULL);
 
     ESP_LOGI(TAG, "== RadioLog Start ==");
     ESP_LOGI(TAG, "ver.%s", vers_tag);
